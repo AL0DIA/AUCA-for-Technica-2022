c6e65fea8907cf9003f6e1cca3606a2968f5bd5b
AL0DIA <hi@alodia.dev>
2022-10-15 20:18:45 +0200

    Afegit tractament imatge (PIL)

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
-
+import urllib.request
+from PIL import Image
 
 if __name__ == '__main__':
 
     genArt1 = requests.post(
         "https://api.deepai.org/api/text2img",
         data={
-            'text': 'cute cat fighting against dinosaurs',
+            'text': 'cute cat fighting against capitalism',
         },
         headers={'api-key': 'df27bb07-656a-4446-8ce6-c3dc50f387a6'}
     )
-    print(genArt1.json())
     art1 = genArt1.json()
-    print(art1["output_url"])
+    art1URL = art1["output_url"]
+
+    urllib.request.urlretrieve(art1URL, "art1.png")
+    img1 = Image.open("art1.png")
+    img1.show()
 
